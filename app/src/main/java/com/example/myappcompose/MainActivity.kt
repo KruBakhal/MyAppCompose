e97da53649b801c8775a3c35804bf55dd924f689
Krunal <kbakhal5@gmail.com>
2024-01-16 18:24:28 +0530

    Learn Simple State  on Click

--- app/src/main/java/com/example/myappcompose/MainActivity.kt
+++ app/src/main/java/com/example/myappcompose/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +18,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -36,6 +40,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myappcompose.ui.theme.MyAppComposeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,8 +59,47 @@ private fun MyApp() {
     Surface(
         modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
     ) {
-        StylingTextExample()
+        LearnState()
     }
+}
+
+@Composable
+fun LearnState() {
+    val colorState = remember {
+        mutableStateOf(Color.Yellow)
+    }
+
+    Column() {
+        BoxItem(
+            Modifier
+                .weight(1f)
+                .fillMaxSize()
+                .background(Color.Black)
+        ) {
+            colorState.value = it
+        }
+        Box(
+            Modifier
+                .weight(1f)
+                .fillMaxSize()
+                .background(colorState.value)
+        )
+    }
+
+}
+
+@Composable
+fun BoxItem(modifier: Modifier = Modifier, colorState: (Color) -> Unit) {
+    Box(modifier = modifier.clickable {
+        colorState.invoke(
+            Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f,
+            )
+        )
+    })
 }
 
 @Composable
